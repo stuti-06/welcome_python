1dc54119ca9ea7e06b1e171a718b42043354af36
stuti-06 <stuti.nigam@capgemini.com>
2025-01-16 14:27:55 +0530

    Update test_arithmatic_operations.py

--- test_arithmatic_operations.py
+++ test_arithmatic_operations.py
@@ -9,18 +9,15 @@ class TestArithmeticOperations(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
 
-
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
         self.assertEqual(subtract(0, 4), -4)
         self.assertEqual(subtract(-3, -6), 3)
 
-
     def test_multiply(self):
         self.assertEqual(multiply(2, 3), 6)
         self.assertEqual(multiply(-1, 5), -5)
         self.assertEqual(multiply(0, 100), 0)
-
 
     def test_divide(self):
         self.assertEqual(divide(10, 2), 5)
